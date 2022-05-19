f3a6036f9a3846ebd65aa5e6460ad76a12b899ca
Manuel Gómez Furones <71091550+ManuGF02@users.noreply.github.com>
2022-05-20 00:36:05 +0200

    Importante para entender el proyecto correctamente

---
+++ Documentación/Insercion de Árbitros.sql
@@ -0,0 +1,20 @@
+INSERT INTO Arbitros VALUES (1, 'Alberola Rojas', 'Javier', '1991/6/22', 'Castilla la Mancha');
+INSERT INTO ARBITROS VALUES (2, 'Cordero Vega', 'Adrián', '1984/06/15', 'Cantabria');
+INSERT INTO ARBITROS VALUES (3, 'Cuadra Fernández', 'Guillermo', '1984/04/25', 'Islas Baleares');
+INSERT INTO ARBITROS VALUES (4, 'De Burgos Bengoetxea', 'Ricardo', '1986/03/16', 'Vizcaya');
+INSERT INTO ARBITROS VALUES (5, 'Del Cerro Grande', 'Carlos', '1976/03/13', 'Madrid');
+INSERT INTO ARBITROS VALUES (6, 'Díaz de Mera Escuderos', 'Isidro', '1989/08/24', 'Castilla la Mancha')
+INSERT INTO ARBITROS VALUES (7, 'Figueroa Vázquez', 'Jorge', '1980/02/05', 'Andalucía');
+INSERT INTO ARBITROS VALUES (8, 'Gil Manzano', 'Jesús', '1984/02/04', 'Extremadura')
+INSERT INTO ARBITROS VALUES (9, 'González Fuertes', 'Pablo', '1980/06/08', 'Asturias')
+INSERT INTO ARBITROS VALUES (10, 'Hernández Hernández', 'Alejandro José', '1982/11/10', 'Canarias');
+INSERT INTO ARBITROS VALUES (11, 'Jaime Latre', 'Santiago')
+INSERT INTO ARBITROS VALUES (12, 'Martínez Munuera', 'Juan');
+INSERT INTO ARBITROS VALUES (13,'Mateu Lahoz', 'Miguel');
+INSERT INTO ARBITROS VALUES (14, 'Melero López', 'Mario')
+INSERT INTO ARBITROS VALUES (15, 'Munuera Montero', 'José Luis');
+INSERT INTO ARBITROS VALUES (16, 'Muñiz Ruiz', 'Alejandro') 
+INSERT INTO ARBITROS VALUES (17, 'Ortiz Arias', 'Miguel Ángel')
+INSERT INTO ARBITROS VALUES (18, 'Pizarro Gómez', 'Valentín')
+INSERT INTO ARBITROS VALUES (19, 'Sánchez Martínez', 'José María')
+INSERT INTO ARBITROS VALUES (20, 'Soto Grado', 'César')
